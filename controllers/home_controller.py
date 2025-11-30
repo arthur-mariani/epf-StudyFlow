902b994c498316677224e4c68e8a12a76fe65e7e
Matheus <matheusrodriguespontes346@gmail.com>
2025-11-30 07:49:15 -0300

    feat(controller): rota de API

--- controllers/home_controller.py
+++ controllers/home_controller.py
@@ -1,30 +1,26 @@
 from bottle import request, response, Bottle
 from .base_controller import BaseController
+from services.sessao_service import SessaoService
 
 class HomeController(BaseController):
     def __init__(self, app):
         super().__init__(app)
+        self.sessao_service = SessaoService()
         self.setup_routes()
 
     def setup_routes(self):
-        # Rota principal após o login
         self.app.route('/home', method='GET', callback=self.exibir_home)
-        
-        # Rota de sair
         self.app.route('/logout', method='GET', callback=self.fazer_logout)
-
+        self.app.route('/salvar_sessao', method='POST', callback=self.salvar_sessao_ajax)
+        
     def exibir_home(self):
-        # 1. VERIFICAÇÃO DE SEGURANÇA
-        # Tenta recuperar o cookie criado no LoginController
-        # ATENÇÃO: O 'secret' deve ser IGUAL ao usado no login_controller.py
         usuario_logado = request.get_cookie("usuario_logado", secret='chave_secreta_do_projeto')
 
         if usuario_logado:
-            # 2. Se tem cookie, mostra a tela Home
-            # Passamos o 'user' para o template poder escrever "Olá, Fulano"
+
             return self.render('home', user=usuario_logado)
         else:
-            # 3. Se não tem cookie, redireciona para o login
+            # Se não tem cookie, redireciona para o login
             return self.redirect('/login')
 
     def fazer_logout(self):
@@ -32,6 +28,24 @@ class HomeController(BaseController):
         response.delete_cookie("usuario_logado")
         return self.redirect('/login')
 
-# Criação da instância para exportar (se você usar esse padrão de exportação)
+    def salvar_sessao_ajax(self):
+        dados = request.json
+        if not dados:
+            return {"status": "erro", "msg": "Sem dados"}
+
+        usuario_gmail = request.get_cookie("usuario_logado", secret='chave_secreta_do_projeto')
+        
+        if not usuario_gmail:
+             response.status = 403
+             return {"status": "erro", "msg": "Não logado"}
+
+        segundos = dados.get('segundos')
+        materia = dados.get('materia')
+        
+        self.sessao_service.salvar_tempo(usuario_gmail, segundos, materia)
+
+        print(f"Salvando sessão: {usuario_gmail} estudou {segundos}s de {materia}")
+        return {"status": "sucesso"}
+    
 home_routes = Bottle()
 home_controller = HomeController(home_routes)
