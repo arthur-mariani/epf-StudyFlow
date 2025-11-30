746d2d154b9744dbe9cb3c05e320c544a405f14f
Matheus <matheusrodriguespontes346@gmail.com>
2025-11-30 07:56:38 -0300

    feat(service): implementa logica de negocio para sessoes

---
+++ services/sessao_service.py
@@ -0,0 +1,24 @@
+from models.sessao import SessaoDAO, Sessao
+
+class SessaoService:
+    def __init__(self):
+        self.sessao_dao = SessaoDAO()
+
+    def salvar_tempo(self, usuario_gmail, segundos, materia):
+
+        todas = self.sessao_dao.listar_todos()
+        if todas:
+
+            novo_id = max(item['id'] for item in todas) + 1
+        else:
+            novo_id = 1
+
+        nova_sessao = Sessao(
+            id=novo_id,
+            usuario_gmail=usuario_gmail,
+            segundos=int(segundos),
+            materia=materia
+        )
+
+        self.sessao_dao.criar(nova_sessao)
+        return True
