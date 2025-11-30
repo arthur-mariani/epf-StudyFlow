d8161d47cec14e343f05f9988418a51a4be9e301
Lucas Araújo Lima <lucasaraujolima929@gmail.com>
2025-11-30 02:55:01 -0300

    chore: registra e integra as rotas de configuracoes ao app principal

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -3,9 +3,11 @@ from controllers.user_controller import user_routes
 from controllers.signin_controller import signin_routes
 from controllers.login_controller import login_routes
 from controllers.home_controller import home_routes
+from .settings_controller import settings_controller
 
 def init_controllers(app: Bottle):
     app.merge(user_routes)
     app.merge(signin_routes)
     app.merge(login_routes)
     app.merge(home_routes)
+    app.merge(settings_controller)
