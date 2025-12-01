6f21379fbe681343846b5cde0ee2949fbb148220
arthur-mariani <arthurmariani.5@gmail.com>
2025-12-01 01:54:49 -0300

    Remoção da users e suas derivadas

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,5 +1,4 @@
 from bottle import Bottle
-from controllers.user_controller import user_routes
 from controllers.signin_controller import signin_routes
 from controllers.login_controller import login_routes
 from controllers.home_controller import home_routes
@@ -7,7 +6,6 @@ from .settings_controller import settings_routes
 from controllers.statistics_controller import statistics_routes
 
 def init_controllers(app: Bottle):
-    app.merge(user_routes)
     app.merge(signin_routes)
     app.merge(login_routes)
     app.merge(home_routes)
